d033d93884441bcc18592b94ca7baf20d782e73b
MCappe <melissacappe@gmail.com>
2020-12-15 15:28:58 -0500

    making and breaking

---
+++ script.js
@@ -0,0 +1,18 @@
+// Namespacing
+// const portfolioSite = {};
+
+// // Initializer function
+
+// portfolioSite.init = () => {
+//   portfolioSite.eventListener();
+// }
+
+// portfolioSite.eventListener = () => {
+  
+// }
+
+// $(function () {
+//   AOS.init({
+//     duration: 1200,
+//   });
+// })
