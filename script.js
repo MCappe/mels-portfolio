93774c758bdb7e30d509150369bf2e7712610a45
MCappe <melissacappe@gmail.com>
2020-12-16 16:27:21 -0500

    burger menu success :tada:

--- script.js
+++ script.js
@@ -1,48 +1,35 @@
-// Namespacing
-const portfolioSite = {};
-
-// Initializer function
-
-portfolioSite.init = () => {
-  portfolioSite.eventListener();
-}
-
-portfolioSite.eventListener = () => {
-  
-}
 
 $(function () {
   AOS.init({
     duration: 1200,
   });
+
+  // Hamburger Menu
+  $('#bars').on('click', () => {
+    console.log('clicked');
+    
+    $('.headerNav2').toggleClass("hidden");
+    $('#bars').toggleClass('highlight');
+  })
   
   // Dark/light toggle
-  const toggle = document.querySelector("#toggle");
+  let toggle = document.querySelector("#toggle");
+  toggle.addEventListener("click", modeSwitch);
+
+  toggle = document.querySelector("#toggle2");
   toggle.addEventListener("click", modeSwitch);
 
   let isLight = true;
 
   function modeSwitch() {
     isLight = !isLight;
-    isLight ? toggle.innerText = "🌞" : toggle.innerText = "🌚";
+    isLight ? toggle.innerText = "Light" : toggle.innerText = "Dark";
     const rootElement = document.body;
     rootElement.classList.toggle("lightMode");
   }
 })
 
-// Hamburger menu
-$(document).ready(function () {
-  $(".menu-toggle").click(function () {
-    $(".menu-toggle").toggleClass("menu-open");
-    $(".menu").toggleClass("active");
-  });
-  $(".link").click(function () {
-    $(".menu-toggle").toggleClass("menu-open");
-    $(".menu").toggleClass("active");
-  });
-});
-
 // Form reset
-document.getElementById("myForm").reset(); 
+// document.getElementById("myForm").reset(); 
 
 
